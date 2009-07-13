9b1a58c71f314aaf956bda47295361113ad224a7
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2009-07-13 11:15:51 +0000

    fixed LGPL template

--- trunk/jtstand/jtstand-ui/src/main/java/com/jtstand/swing/AbstractTestSequenceInstanceListTableModel.java
+++ trunk/jtstand/jtstand-ui/src/main/java/com/jtstand/swing/AbstractTestSequenceInstanceListTableModel.java
@@ -14,7 +14,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with GTStand.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.jtstand.swing;
 
