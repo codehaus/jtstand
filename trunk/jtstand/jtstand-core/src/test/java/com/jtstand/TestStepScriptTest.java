389fdfceb2f115953ac3e7925d1eb4ccc361fe72
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2009-08-25 04:42:53 +0000

    jsr223

--- trunk/jtstand/jtstand-core/src/test/java/com/jtstand/TestStepScriptTest.java
+++ trunk/jtstand/jtstand-core/src/test/java/com/jtstand/TestStepScriptTest.java
@@ -18,6 +18,8 @@ public class TestStepScriptTest extends TestCase {
     public static final String FOO = "class Foo  { int getResult() { return 1 } }";
     public static final String GROOVY_SCRIPT = "Foo f = new Foo(); f.getResult() + r";
     public static final String JS_SCRIPT = "var f = new Packages.Foo(); f.getResult() + r";
+    public static final String RUBY_SCRIPT = "require 'java' \n include_class 'Foo' \n f = Foo.new \n r + f.getResult()";
+
     public static GroovyClassLoader gcl = new GroovyClassLoader(Thread.currentThread().getContextClassLoader()) {
 
         @Override
@@ -78,6 +80,6 @@ public class TestStepScriptTest extends TestCase {
 
         ScriptEngine engine = factory.getEngineByName("jruby");
         engine.put("r", 1);
-        assertEquals(2, engine.eval(GROOVY_SCRIPT));
+        assertEquals(2, engine.eval(RUBY_SCRIPT));
     }
 }
