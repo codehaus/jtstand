26479a7e3f96141672569d0a0ba463916e832019
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2009-08-25 04:31:45 +0000

    jsr223

--- trunk/jtstand/jtstand-core/src/test/java/com/jtstand/TestStepScriptTest.java
+++ trunk/jtstand/jtstand-core/src/test/java/com/jtstand/TestStepScriptTest.java
@@ -8,6 +8,7 @@ import groovy.lang.Binding;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyShell;
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import junit.framework.TestCase;
@@ -15,7 +16,8 @@ import junit.framework.TestCase;
 public class TestStepScriptTest extends TestCase {
 
     public static final String FOO = "class Foo  { int getResult() { return 1 } }";
-    public static final String SCRIPT = "Foo f = new Foo(); f.getResult() + r";
+    public static final String GROOVY_SCRIPT = "Foo f = new Foo(); f.getResult() + r";
+    public static final String JS_SCRIPT = "var f = new Packages.Foo(); f.getResult() + r";
     public static GroovyClassLoader gcl = new GroovyClassLoader(Thread.currentThread().getContextClassLoader()) {
 
         @Override
@@ -30,14 +32,52 @@ public class TestStepScriptTest extends TestCase {
     public void testClassLoaderGroovyShell() throws ScriptException {
         Binding binding = new Binding();
         binding.setVariable("r", 1);
-        assertEquals(2, (new GroovyShell(gcl, binding)).parse(SCRIPT).run());
+        assertEquals(2, (new GroovyShell(gcl, binding)).parse(GROOVY_SCRIPT).run());
     }
 
-    public void testClassLoaderJSR223() throws ScriptException{
+    public void testListEngines() throws ScriptException {
+        ScriptEngineManager factory = new ScriptEngineManager(gcl);
+        for (ScriptEngineFactory sef : factory.getEngineFactories()) {
+            System.out.println("engine name: " + sef.getEngineName());
+            for (String name : sef.getNames()) {
+                System.out.println("name: " + name);
+            }
+        }
+    }
+
+    public void testClassLoaderJSR223Groovy() throws ScriptException {
         ScriptEngineManager factory = new ScriptEngineManager();
         Thread.currentThread().setContextClassLoader(gcl);
+
         ScriptEngine engine = factory.getEngineByName("groovy");
         engine.put("r", 1);
-        assertEquals(2, engine.eval(SCRIPT));
+        assertEquals(2, engine.eval(GROOVY_SCRIPT));
     }
+
+    public void testClassLoaderJSR223Js() throws ScriptException {
+        ScriptEngineManager factory = new ScriptEngineManager();
+        Thread.currentThread().setContextClassLoader(gcl);
+
+        ScriptEngine engine = factory.getEngineByName("js");
+        engine.put("r", 1);
+        assertEquals(2.0, engine.eval(JS_SCRIPT));
+    }
+
+//    public void testClassLoaderJSR223Python() throws ScriptException {
+//        ScriptEngineManager factory = new ScriptEngineManager();
+//        Thread.currentThread().setContextClassLoader(gcl);
+//
+//        ScriptEngine engine = factory.getEngineByName("python");
+//        engine.put("r", 1);
+//        assertEquals(2, engine.eval(GROOVY_SCRIPT));
+//    }
+//
+//    public void testClassLoaderJSR223Jruby() throws ScriptException {
+//        ScriptEngineManager factory = new ScriptEngineManager();
+//        Thread.currentThread().setContextClassLoader(gcl);
+//
+//        ScriptEngine engine = factory.getEngineByName("jruby");
+//        engine.put("r", 1);
+//        assertEquals(2, engine.eval(GROOVY_SCRIPT));
+//    }
 }
