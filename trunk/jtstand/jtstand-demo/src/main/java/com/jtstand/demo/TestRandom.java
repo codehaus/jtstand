5507c89fe2d3f67a3d6a775efb5b7015208fa385
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2009-09-01 11:46:33 +0000

    declassification

--- trunk/jtstand/jtstand-demo/src/main/java/com/jtstand/demo/TestRandom.java
+++ trunk/jtstand/jtstand-demo/src/main/java/com/jtstand/demo/TestRandom.java
@@ -4,7 +4,7 @@
  */
 package com.jtstand.demo;
 
-import com.jtstand.TestStepInstance;
+import com.jtstand.StepInterface;
 import java.util.Random;
 
 /**
@@ -14,14 +14,14 @@ import java.util.Random;
 public class TestRandom implements Runnable {
 
     public static Random r = new Random();
-    public static final String STR_AVERAGE = "AVERAGE";
-    public static final String STR_SIGMA = "SIGMA";
-    public static final String[][][] ps = {{{}, {STR_AVERAGE, STR_SIGMA}}};
-    public static final String UNIT = "V";
-    private TestStepInstance step;
+    private StepInterface step;
+
+    public TestRandom(StepInterface step) {
+        this.step = step;
+    }
 
     @Override
     public void run() {
-        step.setValue(step.getPropertyDouble(STR_AVERAGE, 0.0) + step.getPropertyDouble(STR_SIGMA, 1.0) * TestRandom.r.nextGaussian());
+        step.setValue(1.0 + TestRandom.r.nextGaussian());
     }
 }
