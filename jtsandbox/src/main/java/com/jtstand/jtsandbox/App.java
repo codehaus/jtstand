5f724be4b1680717eff4b2e5af7332c830d833e2
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2009-11-30 13:43:36 +0000

    add sandbox

---
+++ jtsandbox/src/main/java/com/jtstand/jtsandbox/App.java
@@ -0,0 +1,13 @@
+package com.jtstand.jtsandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
