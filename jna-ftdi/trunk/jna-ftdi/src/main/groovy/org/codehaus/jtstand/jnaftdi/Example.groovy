a60542a749d533694d8601d5c8ed5d4bf936a855
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2010-03-10 02:17:56 +0000

    jna-ftdi skeleton added

---
+++ jna-ftdi/trunk/jna-ftdi/src/main/groovy/org/codehaus/jtstand/jnaftdi/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package org.codehaus.jtstand.jnaftdi
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
