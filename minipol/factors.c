7176c27949e8fc0fce603818169ce0e4d46d1f0c
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2010-04-21 14:02:11 +0000

    factor to use gsl_vector_int

--- minipol/factors.c
+++ minipol/factors.c
@@ -1,68 +1,75 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <gsl/gsl_vector_int.h>
 
-int factorScalarToVector(int* f, int n, int s, int* a) {
+int factorScalarToVector(const gsl_vector_int* f, int s, int* a) {
     int i;
-    
-    for (i = n - 1; i >= 0; i--) {
-        a[i] = s % f[i];
-        s /= f[i];
+    int fi;
+    for (i = f->size - 1; i >= 0; i--) {
+        fi = f->data[i];
+        a[i] = s % fi;
+        s /= fi;
     }
     return s; //error if not zero
 }
 
-int factorVectorToScalar(int* f, int n, int* a) {
+int factorVectorToScalar(const gsl_vector_int* f, int* a) {
     int i;
     int retval = 0;
 
-    for (i = 0; i < n; i++) {
-        retval *= f[i];
+    for (i = 0; i < f->size; i++) {
+        retval *= f->data[i];
         retval += a[i];
     }
     return retval;
 }
 
-int factorMulti(int* f, int n) {
+int factorMulti(const gsl_vector_int* f) {
+    int i;
     int retval = 1;
 
-    for (n--; n >= 0; n--) {
-        retval *= f[n];
+    for (i = f->size - 1; i >= 0; i--) {
+        retval *= f->data[i];
     }
     return retval;
 }
 
 int factorTest(void) {
     int i;
-    int f[] = {2, 4, 5};
+    gsl_vector_int* f = gsl_vector_int_alloc(3);
+    gsl_vector_int_set(f, 0, 2);
+    gsl_vector_int_set(f, 1, 4);
+    gsl_vector_int_set(f, 2, 5);
+
     int n = 3;
-    int m = factorMulti(f, 3);
+    int m = factorMulti(f);
     printf("m:%d\r\n", m);
 
     int a[3];
     int s = 1;
-    factorScalarToVector(f, n, s, a);
+    factorScalarToVector(f, s, a);
     printf("s:%d\r\n", s);
     for (i = 0; i < n; i++) {
         printf("a[%d]:%d\r\n", i, a[i]);
     }
-    s = factorVectorToScalar(f, n, a);
+    s = factorVectorToScalar(f, a);
     printf("s:%d\r\n\r\n", s);
 
     s = 39;
-    factorScalarToVector(f, n, s, a);
+    factorScalarToVector(f, s, a);
     printf("s:%d\r\n", s);
     for (i = 0; i < n; i++) {
         printf("a[%d]:%d\r\n", i, a[i]);
     }
-    s = factorVectorToScalar(f, n, a);
+    s = factorVectorToScalar(f, a);
     printf("s:%d\r\n\r\n", s);
 
     s = 25;
-    factorScalarToVector(f, n, s, a);
+    factorScalarToVector(f, s, a);
     printf("s:%d\r\n", s);
     for (i = 0; i < n; i++) {
         printf("a[%d]:%d\r\n", i, a[i]);
     }
-    s = factorVectorToScalar(f, n, a);
+    s = factorVectorToScalar(f, a);
     printf("s:%d\r\n\r\n", s);
 }
