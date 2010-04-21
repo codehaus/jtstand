aa46c3db357f56b10246fc935acd446d124bd604
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2010-04-21 11:43:34 +0000

    polifunc

---
+++ minipol/polifunc.c
@@ -0,0 +1,32 @@
+#include <gsl/gsl_vector_double.h>
+#include <math.h>
+#include "polifunc.h"
+
+double polifunc_f(const gsl_vector *v, void *params) {
+    int i;
+    double f = -1.0;
+
+    double *p = (double *) params;
+
+    for (i = 0; i < v->size; i++) {
+        f += gsl_vector_get(v, i) * p[i];
+    }
+    return (f >= 0) ? f : -f; //abs
+}
+
+void polifunc_df(const gsl_vector *v, void *params,
+                 gsl_vector *df) {
+    int i;
+    double *p = (double *) params;
+    double f = polifunc_f(v, params);
+    if (f >= 0) {
+        for (i = 0; i < v->size; i++) {
+            gsl_vector_set(df, i, p[i]);
+        }
+
+    } else {
+        for (i = 0; i < v->size; i++) {
+            gsl_vector_set(df, i, -p[i]);
+        }
+    }
+}
