db26581650772c70e3f2b2cca202b8179a977017
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2010-04-23 03:00:40 +0000

    fixed compiler warnings

---
+++ minipol/nbproject/Makefile-variables.mk
@@ -0,0 +1,40 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# DebugLinux64 configuration
+CND_PLATFORM_DebugLinux64=GNU-Linux-x86
+CND_ARTIFACT_DIR_DebugLinux64=dist/DebugLinux64/GNU-Linux-x86
+CND_ARTIFACT_NAME_DebugLinux64=minipol
+CND_ARTIFACT_PATH_DebugLinux64=dist/DebugLinux64/GNU-Linux-x86/minipol
+CND_PACKAGE_DIR_DebugLinux64=dist/DebugLinux64/GNU-Linux-x86/package
+CND_PACKAGE_NAME_DebugLinux64=minipol.tar
+CND_PACKAGE_PATH_DebugLinux64=dist/DebugLinux64/GNU-Linux-x86/package/minipol.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=minipol
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/minipol
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=minipol.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/minipol.tar
+# DebugLinux32 configuration
+CND_PLATFORM_DebugLinux32=GNU-Linux-x86
+CND_ARTIFACT_DIR_DebugLinux32=dist/DebugLinux32/GNU-Linux-x86
+CND_ARTIFACT_NAME_DebugLinux32=minipol
+CND_ARTIFACT_PATH_DebugLinux32=dist/DebugLinux32/GNU-Linux-x86/minipol
+CND_PACKAGE_DIR_DebugLinux32=dist/DebugLinux32/GNU-Linux-x86/package
+CND_PACKAGE_NAME_DebugLinux32=minipol.tar
+CND_PACKAGE_PATH_DebugLinux32=dist/DebugLinux32/GNU-Linux-x86/package/minipol.tar
+# DebugWin32 configuration
+CND_PLATFORM_DebugWin32=
+CND_ARTIFACT_DIR_DebugWin32=dist/DebugWin32/
+CND_ARTIFACT_NAME_DebugWin32=minipol
+CND_ARTIFACT_PATH_DebugWin32=dist/DebugWin32//minipol
+CND_PACKAGE_DIR_DebugWin32=dist/DebugWin32//package
+CND_PACKAGE_NAME_DebugWin32=minipol.tar
+CND_PACKAGE_PATH_DebugWin32=dist/DebugWin32//package/minipol.tar
