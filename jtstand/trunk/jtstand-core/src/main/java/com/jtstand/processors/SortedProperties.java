a3b0c80cb0b913b7bfa78029f9990b55d226c44e
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2012-10-12 17:16:01 +0000

    add sample report printer

--- jtstand/trunk/jtstand-core/src/main/java/com/jtstand/processors/SortedProperties.java
+++ jtstand/trunk/jtstand-core/src/main/java/com/jtstand/processors/SortedProperties.java
@@ -8,24 +8,33 @@ package com.jtstand.processors;
  *
  * @author albert_kurucz
  */
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Vector;
 
 public class SortedProperties extends Properties {
 
+    private Vector mykeys = new Vector();
+
     /**
      * Overrides, called by the store method.
      */
     @Override
     public synchronized Enumeration keys() {
-        Enumeration keysEnum = super.keys();
-        Vector keyList = new Vector();
-        while (keysEnum.hasMoreElements()) {
-            keyList.add(keysEnum.nextElement());
-        }
-        Collections.sort(keyList);
-        return keyList.elements();
+        return mykeys.elements();
     }
+
+    @Override
+    public synchronized Object setProperty(String key, String value) {
+        if (!mykeys.contains(key)) {
+            mykeys.add(key);
+        }
+        return super.setProperty(key, value);
+    }
+
+    @Override
+    public synchronized Object remove(Object key) {
+        mykeys.remove(key);
+        return super.remove(key);
+    }       
 }
