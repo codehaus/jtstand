a8f9a1ecd5fb029121ccb4fea10958fde643a487
ali65 <ali65@67fca5eb-c769-0410-9c34-ca98404000b0>
2014-01-23 15:16:02 +0000

    upgrade svnkit from 1.8.2 to 1.8.3 engage in 1.7 compatibility

--- jtstand/trunk/jtstand-demo/src/main/resources/jtstand.bat
+++ jtstand/trunk/jtstand-demo/src/main/resources/jtstand.bat
@@ -1,1 +1,1 @@
-java -Xmx256m -Xms256m -Xmn64m -Dorg.jboss.logging.provider=log4j -jar ${project.artifactId}-${project.version}.jar -x jtstand-1.0.xsd
+java -Xmx256m -Xms256m -Xmn64m -Dorg.jboss.logging.provider=log4j -Dsvnkit.wc.17.enabled=true -jar ${project.artifactId}-${project.version}.jar -x jtstand-1.0.xsd
